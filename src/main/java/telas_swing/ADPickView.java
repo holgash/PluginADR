cdf3d22e9abe4626b5fb103709578e23dc76e577
Renato C Domingues <renatocdomingues@outlook.com>
2019-10-01 22:09:30 -0300

    Tentativa de botar Drag and Drop

--- src/main/java/telas_swing/ADPickView.java
+++ src/main/java/telas_swing/ADPickView.java
@@ -2,92 +2,94 @@ package telas_swing;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
-import java.awt.Container;
-import java.util.ArrayList;
-import java.util.List;
+import java.awt.dnd.DropTarget;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
-import javax.swing.JComboBox;
+//import javax.swing.JComboBox;
+import javax.swing.JComponent;
 import javax.swing.JPanel;
-import javax.swing.JTable;
-import javax.swing.table.DefaultTableModel;
+import javax.swing.JTextField;
+import javax.swing.TransferHandler;
 
-import com.change_vision.jude.api.inf.AstahAPI;
-import com.change_vision.jude.api.inf.exception.ProjectNotFoundException;
-import com.change_vision.jude.api.inf.model.IActivityDiagram;
-import com.change_vision.jude.api.inf.model.IClass;
-import com.change_vision.jude.api.inf.model.IDiagram;
-import com.change_vision.jude.api.inf.model.IModel;
-import com.change_vision.jude.api.inf.model.INamedElement;
-import com.change_vision.jude.api.inf.model.IPackage;
-import com.change_vision.jude.api.inf.project.ModelFinder;
-import com.change_vision.jude.api.inf.project.ProjectAccessor;
+//import com.change_vision.jude.api.inf.project.ProjectAccessor;
 import com.change_vision.jude.api.inf.project.ProjectEvent;
 import com.change_vision.jude.api.inf.project.ProjectEventListener;
 import com.change_vision.jude.api.inf.ui.IPluginExtraTabView;
 import com.change_vision.jude.api.inf.ui.ISelectionListener;
 
-@SuppressWarnings({ "unchecked", "rawtypes" })
+@SuppressWarnings({ "serial" })
 public class ADPickView extends JPanel implements IPluginExtraTabView, ProjectEventListener{
 
-	//private DefaultTableModel model = new DefaultTableModel();
-	//private JTable table = new JTable(model);
-	private IModel currentProject;
-	private JComboBox diagram1 = new JComboBox();
-	private JComboBox diagram2 = new JComboBox();
+	//private JComboBox diagram1 = new JComboBox();
+	//private JComboBox diagram2 = new JComboBox();
 	private JButton refinar = new JButton("Refinar Diagramas");
+	//private ProjectAccessor projectAccessor;
+	private MouseListener Mouse = new MouseListener() {
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			JComponent componente = (JComponent) e.getSource();
+			TransferHandler th = componente.getTransferHandler();
+			th.exportAsDrag(componente, e, TransferHandler.COPY);
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	};
+	
+	
+		private JTextField Diagrama1;
+	private JTextField Diagrama2;
 	
 	public ADPickView() {
 		initComponents();
 	}
 	
 	private void initComponents() {
-		//diagram1.setModel(new DefaultComboBoxModel(new String[] {"diagrama 1", "diagrama 2"}));
-		//diagram2.setModel(new DefaultComboBoxModel(new String[] {"diagrama 1", "diagrama 2"}));
 		setLayout(new BorderLayout());
-		add(diagram1,BorderLayout.WEST);
-		add(diagram2,BorderLayout.EAST);
+		Diagrama1 = new JTextField();
+		Diagrama1.setAlignmentX(LEFT_ALIGNMENT);
+		Diagrama2 = new JTextField();
+		Diagrama2.setAlignmentX(RIGHT_ALIGNMENT);
+		add(Diagrama1,BorderLayout.WEST);
+		add(Diagrama2,BorderLayout.EAST);
+		
+		Diagrama1.addMouseListener(Mouse);
+		Diagrama2.addMouseListener(Mouse);
+		
+		Diagrama1.setText("Diagrama 1");
+		Diagrama2.setText("Diagrama 2");	
 		add(refinar,BorderLayout.SOUTH);
-		addProjectEventListener();
+		
 	}
 	
-   /*
-   private void updateTable() {
-	   model.getDataVector().clear();
-	   List<IClass> classList = new ArrayList<IClass>();
-	   getAllClasses(currentProject,classList);
-	   for (IClass cl : classList) {
-          String name = cl.getName();
-          String upperCase = name.toUpperCase();
-          if (name.charAt(0) != upperCase.charAt(0)) {
-        	  model.addRow(new Object[]{name, 
-              Activator.getLabel("validationErrorMessageFirstLetter")});
-          } else if (upperCase.equals(name)) {
-        	  model.addRow(new Object[]{name, 
-              Activator.getLabel("validationErrorMessageAcronym")});
-          } else if (name.contains(" ")) {
-        	  model.addRow(new Object[]{name, 
-              Activator.getLabel("validationErrorMessageBlankSpace")});
-          }
-	   }
-   }
-   
-   
-   
-	private void getAllClasses(INamedElement element, List<IClass> classList) {
-		if (element instanceof IPackage) {
-	       for (INamedElement ownedNamedElement : ((IPackage) element).getOwnedElements()) {
-	    	   getAllClasses(ownedNamedElement, classList);
-	       }
-	     } else if (element instanceof IClass) {
-	    	 classList.add((IClass) element);
-	     }
-	}*/
-	
-	private void updateComboBoxes() {
+	/*private void updateComboBoxes() {
 		List<IActivityDiagram> diagramList = new ArrayList<IActivityDiagram>();
-		getAllDiagrams(currentProject,diagramList);
+		getDiagrams(diagramList);
 		List<String> names = new ArrayList<String>();
 		for(IActivityDiagram dg: diagramList) {
 			 names.add(dg.getName());
@@ -100,56 +102,38 @@ public class ADPickView extends JPanel implements IPluginExtraTabView, ProjectEv
 			diagram1.setModel(new DefaultComboBoxModel(new String[] {"Não Existem Diagramas"}));
 			diagram2.setModel(new DefaultComboBoxModel(new String[] {"Não Existem Diagramas"}));
 		}
-	}
+	}*/
 	
-	private void getAllDiagrams(INamedElement element, List<IActivityDiagram> diagramList) {
-		/*if (element instanceof IPackage) {
-	       for (INamedElement ownedNamedElement : ((IPackage) element).getOwnedElements()) {
-	    	   getAllDiagrams(ownedNamedElement, diagramList);
-	       }
-	     } else if (element instanceof IActivityDiagram) {
-	    	 diagramList.add((IActivityDiagram) element);
-	     }*/
-		
+	/*private void getDiagrams(List<IActivityDiagram> diagramList) {
 		try {
-			INamedElement[] findElements = findElements((ProjectAccessor) currentProject);
+			projectAccessor = AstahAPI.getAstahAPI().getProjectAccessor();
+			INamedElement[] findElements = findElements(projectAccessor);
 			for (int i = 0; i < findElements.length; i++) {
 				if (findElements[i] instanceof IActivityDiagram) {
 					diagramList.add((IActivityDiagram) findElements[i]);
 				}
 			}
-		} catch (ProjectNotFoundException e) {
+		}catch (ProjectNotFoundException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-	}
+	}*/
 	
-	private static INamedElement[] findElements(ProjectAccessor projectAccessor) throws ProjectNotFoundException {
+	/*private static INamedElement[] findElements(ProjectAccessor projectAccessor) throws ProjectNotFoundException {
 		INamedElement[] foundElements = projectAccessor.findElements(new ModelFinder() {
 			public boolean isTarget(INamedElement namedElement) {
 				return namedElement instanceof IActivityDiagram;
 			}
 		});
 		return foundElements;
-	}
+	}*/
 
-	private void addProjectEventListener() {
-		try {
-			AstahAPI api = AstahAPI.getAstahAPI();
-			ProjectAccessor projectAcessor = api.getProjectAccessor();
-			projectAcessor.addProjectEventListener(this);
-			currentProject = projectAcessor.getProject();
-		} catch (ProjectNotFoundException e) {
-			System.out.println(Activator.getLabel("projectNoOpenedTitle"));
-	    } catch (ClassNotFoundException e) {
-	    	System.out.println(Activator.getLabel("unexpectedError"));
-	    }
-	}
 
 	@Override
 	public void activated() {
-		updateComboBoxes();
+		//updateComboBoxes();
 		
 	}
 
@@ -172,18 +156,18 @@ public class ADPickView extends JPanel implements IPluginExtraTabView, ProjectEv
 
 	@Override
 	public String getDescription() {
-		// TODO Auto-generated method stub
-		return null;
+		return Activator.getLabel("viewDescription");
 	}
 
 	@Override
 	public String getTitle() {
 		return Activator.getLabel("viewTitle");
+
 	}
 
 	@Override
 	public void projectChanged(ProjectEvent arg0) {
-		updateComboBoxes();
+		//updateComboBoxes();
 		
 	}
 
